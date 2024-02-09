49b9ca7f2b8e92280d09d65d98710ac222b138da
Rin <shinohara-rin@users.noreply.github.com>
2024-02-09 16:35:36 +0800

    cleanup

--- main.js
+++ main.js
@@ -3,55 +3,56 @@ import { Terminal } from "xterm";
 import { WebLinksAddon } from "xterm-addon-web-links";
 import { CanvasAddon } from "xterm-addon-canvas";
 import { FitAddon } from "xterm-addon-fit";
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 import Split from "split.js";
+
+const theme = {
+  background: "#000000",
+  foreground: "#ffffff",
+  cursor: "#ffffff",
+  cursorAccent: "#000000",
+  selection: "rgba(255, 255, 255, 0.3)",
+  black: "#000000",
+  red: "#ff0000",
+  green: "#00ff00",
+  yellow: "#ffff00",
+  blue: "#0000ff",
+  magenta: "#ff00ff",
+  cyan: "#00ffff",
+  white: "#ffffff",
+  brightBlack: "#808080",
+  brightRed: "#ff0000",
+  brightGreen: "#00ff00",
+  brightYellow: "#ffff00",
+  brightBlue: "#0000ff",
+  brightMagenta: "#ff00ff",
+  brightCyan: "#00ffff",
+  brightWhite: "#ffffff",
+
+  // Define styles for extended ANSI escape sequences (16-255)
+  extendedAnsi: (() => {
+    const extendedAnsi = {};
+
+    for (let i = 16; i <= 255; i++) {
+      // Calculate the corresponding RGB values for each index This calculation is
+      // based on the xterm 256-color palette formula
+      const colorIndex = i - 16;
+      const r = Math.floor(colorIndex / 36) * 51;
+      const g = Math.floor((colorIndex % 36) / 6) * 51;
+      const b = (colorIndex % 6) * 51;
+
+      const color = `#${r.toString(16).padStart(2, "0")}${g
+        .toString(16)
+        .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
+      extendedAnsi[i.toString()] = { color };
+    }
+
+    return extendedAnsi;
+  })(),
+}
 
 window.addEventListener("DOMContentLoaded", function () {
   Split(["#webpage", "#terminal"], { direction: "vertical" });
-  const theme = {
-    background: "#000000",
-    foreground: "#ffffff",
-    cursor: "#ffffff",
-    cursorAccent: "#000000",
-    selection: "rgba(255, 255, 255, 0.3)",
-    black: "#000000",
-    red: "#ff0000",
-    green: "#00ff00",
-    yellow: "#ffff00",
-    blue: "#0000ff",
-    magenta: "#ff00ff",
-    cyan: "#00ffff",
-    white: "#ffffff",
-    brightBlack: "#808080",
-    brightRed: "#ff0000",
-    brightGreen: "#00ff00",
-    brightYellow: "#ffff00",
-    brightBlue: "#0000ff",
-    brightMagenta: "#ff00ff",
-    brightCyan: "#00ffff",
-    brightWhite: "#ffffff",
-
-    // Define styles for extended ANSI escape sequences (16-255)
-    extendedAnsi: (() => {
-      const extendedAnsi = {};
-
-      for (let i = 16; i <= 255; i++) {
-        // Calculate the corresponding RGB values for each index This calculation is
-        // based on the xterm 256-color palette formula
-        const colorIndex = i - 16;
-        const r = Math.floor(colorIndex / 36) * 51;
-        const g = Math.floor((colorIndex % 36) / 6) * 51;
-        const b = (colorIndex % 6) * 51;
-
-        const color = `#${r.toString(16).padStart(2, "0")}${g
-          .toString(16)
-          .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
-        extendedAnsi[i.toString()] = { color };
-      }
-
-      return extendedAnsi;
-    })(),
-  };
   var term = new Terminal({
     rows: 24,
     cols: 80,
@@ -65,6 +66,13 @@ window.addEventListener("DOMContentLoaded", function () {
   term.open(document.getElementById("terminal"));
   term.loadAddon(new CanvasAddon());
   fitAddon.fit();
+
+  this.document.getElementById('reconnect').addEventListener('click', () => {
+    socket.disconnect()
+    term.reset()
+    socket.connect()
+  })
+
   const term_resize_ob = new ResizeObserver((entries) => {
     try {
       fitAddon && fitAddon.fit();
@@ -73,6 +81,7 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   });
   term_resize_ob.observe(document.getElementById("terminal"));
+
   term.write("Hello! ");
   const socket = io("ws://65.109.173.151:3000/")
   socket.on('connect', () => {
@@ -85,7 +94,7 @@ window.addEventListener("DOMContentLoaded", function () {
   socket.on('data', (data) => {
     term.write(data)
   })
-  socket.on('exit',(evt)=>{
+  socket.on('exit', (evt) => {
     term.write("Process exited with code " + evt.exitCode)
     socket.disconnect()
   })
@@ -94,10 +103,5 @@ window.addEventListener("DOMContentLoaded", function () {
       cols: evt.cols,
       rows: evt.rows
     })
-  })
-  this.document.getElementById('reconnect').addEventListener('click', () => {
-    socket.disconnect()
-    term.reset()
-    socket.connect()
   })
 });
