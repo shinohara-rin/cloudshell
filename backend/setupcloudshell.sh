98e8ef67da99f4590eeb757ec3b75e536e691cb2
Rin <shinohara-rin@users.noreply.github.com>
2024-03-09 22:02:32 +0800

    download and setup qemu image

--- backend/setupcloudshell.sh
+++ backend/setupcloudshell.sh
@@ -1,12 +1,33 @@
-#!/bin/bash
+#!/bin/sh
+# Setup cloudshell environment from a brandnew image
+
+# Install necessary packages
+pkg64 install -y llvm-morello vim emacs nano
+pkg64c install -y bash tmux curl wget zip unzip git
+
+# Create cloudshell user if it doesn't exist
+if ! pw usershow cloudshell >/dev/null 2>&1; then
+    mkdir /home/cloudshell
+    pw user add -n cloudshell -c "Cloud Shell User" -d /home/cloudshell -m -s /usr/local/bin/bash
+    chown -R cloudshell /home/cloudshell
+fi
+
+# Setup sshd
+sed -i '' 's/#PasswordAuth/PasswordAuth/' /etc/ssh/sshd_config
 
 chmod o-r /tmp
 chmod -R a-w ~cloudshell
 chown -R root:wheel ~cloudshell
-chmod a+r ~cloudshell/.ssh/authorized_keys
+mkdir ~cloudshell/.ssh/
+cp /root/.ssh/authorized_keys ~cloudshell/.ssh/
+chmod 444 ~cloudshell/.ssh/authorized_keys
 cat > ~cloudshell/.bashrc <<EOF
 force_color_prompt=yes
 mkdir \$TEMP_HOME
 cd \$TEMP_HOME
 export HOME=\$TEMP_HOME
 EOF
+chmod a+x ~cloudshell/.bashrc
+
+# Make self executable for rc
+chmod +x /etc/rc.d/setupcloudshell.sh
